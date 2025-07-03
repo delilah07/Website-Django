697b2f95c7276bc84636629e5f9ef11eca919534
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2025-07-03 22:50:09 +0200

    add profile

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogapp',
     'users',
-    # 'blogapp.apps.BlogappConfig'
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
